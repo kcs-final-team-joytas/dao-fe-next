c8a251cc4892f3b988d18a4fe96e587a0dee14d3
erica0321 <hyunahrho29@gmail.com>
2024-11-16 16:11:19 +0900

    feat: 라운지 목록 페이지 추가

--- src/app/lounges/page.tsx
+++ src/app/lounges/page.tsx
@@ -1,12 +1,38 @@
-import Image from 'next/image'
+import Layout from '@components/Layout'
 import styles from './page.module.css'
-import Layout from '@/components/Layout'
+import { Suspense, lazy } from 'react'
+import rocket from '@images/rocket.webp'
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
 
-export default function Lounges() {
+const LoungeContainer = lazy(() => import('./components/LoungeContainer'))
+const LoadingLottie = dynamic(
+  () => import('@components/lotties/LoadingLottie'),
+  {
+    ssr: false,
+  }
+)
+
+export default function LoungeListPage() {
   return (
     <Layout>
-      <div className={styles.page}>
-        <main className={styles.main}>hi</main>
+      <div className={styles.globalContainer32}>
+        <div className={styles.globalTitle}>
+          <Image
+            src={rocket}
+            alt='rocket'
+            style={{ width: '20px', marginRight: '10px' }}
+          />
+          추억할 라운지를 보여드릴게요
+        </div>
+        <div className={styles.globalSubTitle}>
+          라운지를 클릭해 입장하거나, 라운지를 생성해보세요!
+        </div>
+        <div className={styles.globalWidth}>
+          <Suspense fallback={<LoadingLottie />}>
+            <LoungeContainer />
+          </Suspense>
+        </div>
       </div>
     </Layout>
   )
