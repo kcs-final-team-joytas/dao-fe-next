72d5131c8fa9f0b4c359709093012989ec5062de
yoouung <jyp101400@gmail.com>
2024-12-11 23:12:10 +0900

    design: 레이아웃 스타일 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import Header from '@/app/Header'
 import Footer from '@/app/Footer'
 import ClientEventSource from '@/utils/clientEvent'
+import styles from '@components/Layout.module.css'
+
 
 export default function Layout({
   children,
@@ -8,10 +10,10 @@ export default function Layout({
   children: React.ReactNode
 }>) {
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div style={{ width: '100%', height: '100vh' }}>
       <ClientEventSource />
       <Header />
-      {children}
+      <div className={styles.childrenDiv}>{children}</div>
       <Footer />
     </div>
   )
