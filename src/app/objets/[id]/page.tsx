bb282508303e007438d98abb30dcd221484769ea
yoouung <jyp101400@gmail.com>
2024-12-16 18:04:59 +0900

    feat: 오브제 react-query 적용

--- src/app/objets/[id]/page.tsx
+++ src/app/objets/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
 import styles from './page.module.css'
 import { useObjetContext } from '@/types/objetContext'
 import { APIs, URL } from '@/static'
@@ -10,6 +9,15 @@ import { toast } from 'react-toastify'
 import GoCommunityBtn from './components/GoCommunityBtn'
 import { ChatMessage } from './components/ChatMessage'
 import Image from 'next/image'
+import { useQuery } from 'react-query'
+import dynamic from 'next/dynamic'
+
+const LoadingLottie = dynamic(
+  () => import('@components/lotties/LoadingLottie'),
+  {
+    ssr: false,
+  }
+)
 
 interface LayoutProps {
   params: { id: string }
@@ -25,54 +33,74 @@ interface Message {
   created_at: string
 }
 
+const fetchChatToken = async (id: string) => {
+  const tokenResponse = await fetch(`${APIs.chat}/${id}/room-token`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+    },
+    credentials: 'include',
+  })
+
+  if (!tokenResponse.ok) {
+    throw new Error('Failed to fetch chat token')
+  }
+
+  const tokenData = await tokenResponse.json()
+  const roomToken = tokenData.data.room_token
+  return roomToken
+}
+
+const fetchChatData = async (roomToken: string) => {
+  const chatResponse = await fetch(
+    `${APIs.chat}/${roomToken}/messages/recent`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+      credentials: 'include',
+    }
+  )
+
+  if (!chatResponse.ok) {
+    throw new Error('Failed to fetch chat messages')
+  }
+
+  const chatData = await chatResponse.json()
+  return chatData
+}
+
 export default function Page({ params }: LayoutProps) {
   const router = useRouter()
   const objetContext = useObjetContext()
   const { objetData, callingPeople = 0 } = objetContext || {}
   const id = params.id
-  const [chatData, setChatData] = useState<Message[]>()
 
   const setChatToken = useObjetStore((state) => state.setChatToken)
 
-  const fetchChatData = async () => {
-    try {
-      const tokenResponse = await fetch(`${APIs.chat}/${id}/room-token`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-        },
-        credentials: 'include',
-      })
+  const { data: chatData, isLoading: isChatLoading } = useQuery(
+    ['chatData', id],
+    async () => {
+      const chatToken = await fetchChatToken(id)
 
-      if (!tokenResponse.ok) {
-        throw new Error('Failed to fetch chat token')
+      setChatToken(chatToken)
+
+      const messagesRes = await fetchChatData(chatToken)
+
+      return {
+        roomToken: chatToken,
+        messages: messagesRes.data.messages,
       }
-
-      const tokenData = await tokenResponse.json()
-      const roomToken = tokenData.data.room_token
-      setChatToken(roomToken)
-
-      const chatResponse = await fetch(
-        `${APIs.chat}/${roomToken}/messages/recent`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-          },
-          credentials: 'include',
-        }
-      )
-
-      if (!chatResponse.ok) {
-        throw new Error('Failed to fetch chat messages')
-      }
-
-      const chatData = await chatResponse.json()
-      setChatData(chatData.data.messages)
-    } catch {
-      toast.error('채팅 가져오기 실패')
+    },
+    {
+      retry: 1,
+      onError: (error) => {
+        console.error('오브제 정보 가져오기 실패: ', error)
+        toast.error('오브제 정보를 가져오지 못했습니다.')
+      },
     }
-  }
+  )
 
   const handleChatClick = () => {
     router.push(`${URL.objet}/${id}/chatting`)
@@ -90,12 +118,16 @@ export default function Page({ params }: LayoutProps) {
     }
   }
 
-  useEffect(() => {
-    fetchChatData()
-  }, [])
+  if (isChatLoading) {
+    return (
+      <div style={{ display: 'flex', height: '100%', alignItems: 'center' }}>
+        <LoadingLottie />
+      </div>
+    )
+  }
 
   return (
-    <div className={styles.container}>
+    <>
       <div className={styles.detailContainer}>
         {objetData?.objet_image ? (
           <Image
@@ -113,10 +145,10 @@ export default function Page({ params }: LayoutProps) {
       </div>
       <div className={styles.divider} />
       <div className={styles.chatPreviewContainer}>
-        <div className={styles.chatHeader}>채팅 미리보기</div>
-        {chatData && chatData.length > 0 ? (
+        <div className={styles.chatHeader}>| &nbsp; 채팅 미리보기</div>
+        {chatData && chatData.messages.length > 0 ? (
           <div className={styles.chattingsWrapper}>
-            {chatData.map((message: Message, index: number) => (
+            {chatData.messages.map((message: Message, index: number) => (
               <ChatMessage
                 key={index}
                 userName={message.sender_name}
@@ -144,6 +176,6 @@ export default function Page({ params }: LayoutProps) {
           />
         </div>
       </div>
-    </div>
+    </>
   )
 }
